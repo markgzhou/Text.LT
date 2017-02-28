fa580ba7df23302b1654a9fc5514ad654a1ad642
Guanlong Zhou <zhouguanlong2012@gmail.com>
2017-02-27 22:17:18 -0600

    change redirect url

--- googleOAuthCommon.php
+++ googleOAuthCommon.php
@@ -1,6 +1,6 @@
 <?php
     $client_id = '218412164541-0pnr9pg1g8995kntltihl38edttlbq55.apps.googleusercontent.com';
     $client_secret = 'LVAxKULf6-e_heCKsJVOIvAG';
-    //$redirect_uri = 'https://lt.gterminal.com/login.php';
-    $redirect_uri = 'http://localhost/Text.LT/login.php';
+    $redirect_uri = 'https://lt.gterminal.com/login.php';
+    //$redirect_uri = 'http://localhost/Text.LT/login.php';
 ?>
