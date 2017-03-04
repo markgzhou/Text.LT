cd2b5fb885afe7f218e8f91a9e0cd70245b873fd
Guanlong Zhou <zhouguanlong2012@gmail.com>
2017-03-03 21:57:02 -0600

    Added debug file and googlaIAuthCoomon file

--- googleOAuthCommon.php
+++ googleOAuthCommon.php
@@ -1,6 +1,9 @@
 <?php
     $client_id = '218412164541-0pnr9pg1g8995kntltihl38edttlbq55.apps.googleusercontent.com';
     $client_secret = 'LVAxKULf6-e_heCKsJVOIvAG';
-    $redirect_uri = 'https://lt.gterminal.com/login.php';
-    //$redirect_uri = 'http://localhost/Text.LT/login.php';
+    //$redirect_uri = 'https://lt.gterminal.com/login.php';
+    $redirect_uri = 'http://localhost/Text.LT/login.php';
+
+    //$landing_page_url = 'https://lt.gterminal.com/mynotes.php';
+    $landing_page_url = 'http://localhost/Text.LT/mynotes.php';
 ?>
