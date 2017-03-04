d819a64607e2336f034ee898a86a9e1a1b923b2a
Guanlong Zhou <zhouguanlong2012@gmail.com>
2017-03-04 01:20:27 -0600

    modified redirect location while creating new notes.

--- googleOAuthCommon.php
+++ googleOAuthCommon.php
@@ -1,8 +1,8 @@
 <?php
     $client_id = '218412164541-0pnr9pg1g8995kntltihl38edttlbq55.apps.googleusercontent.com';
     $client_secret = 'LVAxKULf6-e_heCKsJVOIvAG';
-    //$environment = 'https://lt.gterminal.com/';
-   $environment = 'http://localhost/Text.LT/';
+    $environment = 'https://lt.gterminal.com/';
+    //$environment = 'http://localhost/Text.LT/';
 
     $redirect_uri = $environment . 'login.php';
 
